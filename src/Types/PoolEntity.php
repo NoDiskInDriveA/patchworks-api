0cf9513f7b611e97abf0a852a47abad9e61daa5e
Patrick Durold <patrick@durold.de>
2024-05-08 10:03:35 +0200

    Add all known pool entity types

--- src/Types/PoolEntity.php
+++ src/Types/PoolEntity.php
@@ -21,14 +21,54 @@ declare(strict_types=1);
 
 namespace Nodiskindrivea\PatchworksApi\Types;
 
+use function str_replace;
 use function strtolower;
 use function ucwords;
 
 enum PoolEntity: int
 {
     case DATA = 1;
-    case ORDER = 37;
-    case PRODUCT = 54;
+    case ORDERS = 37;
+    case ORDER_LINES = 38;
+    case ORDER_TAX_LINES = 39;
+    case ORDER_LINE_ITEM_DISCOUNTS = 40;
+    case ORDER_LINE_ITEMS_TAX_LINES = 41;
+    case DISCOUNT_DETAILS = 42;
+    case SHIPPING_ADDRESS = 43;
+    case SHIPPING_LINE_DISCOUNTS = 44;
+    case SHIPPING_LINES = 45;
+    case SHIPPING_LINE_TAX_LINES = 46;
+    case CUSTOMER = 47;
+    case REFUNDS = 48;
+    case REFUND_LINE_ITEMS = 49;
+    case REFUNDS_ORDER_ADJUSTMENT = 50;
+    case REFUND_LINE_ITEM_DISCOUNTS = 51;
+    case REFUND_LINE_ITEM_TAX_LINES = 52;
+    case REFUND_TRANSACTIONS = 53;
+    case PRODUCTS = 54;
+    case VARIANT = 55;
+    case IMAGES = 56;
+    case FULFILLMENTS_LINE_ITEMS = 57;
+    case FULFILLMENTS = 58;
+    case INVENTORY_ITEMS = 59;
+    case INVENTORY_LEVELS = 60;
+    case LOCATIONS = 61;
+    case GIFTCARDS = 62;
+    case RETURN = 63;
+    case PURCHASE_ORDERS = 64;
+    case STOCK = 65;
+    case REPORTS = 66;
+    case CANCELLATIONS = 67;
+    case CHANNELS = 68;
+    case INVOICES = 69;
+    case TRANSFER_ORDERS = 70;
+    case BILLING_ADDRESS = 71;
+    case ADDRESSES = 72;
+    case CONTACTS = 73;
+    case CATEGORIES = 74;
+    case BRANDS = 75;
+    case SITE = 76;
+    case CHANNEL = 77;
 
     /**
      * @return string
@@ -36,10 +76,8 @@ enum PoolEntity: int
     public function title(): string
     {
         return match ($this) {
-            self::DATA => 'Data',
-            self::ORDER => 'Order',
-            self::PRODUCT => 'Product',
-            default => ucwords(strtolower($this->name))
+            self::DATA => 'Unspecified (TX?)',
+            default => ucwords(strtolower(str_replace('_', ' ', $this->name)))
         };
     }
 }
